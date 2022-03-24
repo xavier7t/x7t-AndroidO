89b4d952b26a1e177eb098af66b721565aea44d4
xavier7t <32859092+xavier7t@users.noreply.github.com>
2022-03-24 19:13:45 -0400

    s2_9_exampleapp

---
+++ s2_9_exampleapp/app/src/main/java/com/x7t/s2_9_exampleapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.x7t.s2_9_exampleapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
