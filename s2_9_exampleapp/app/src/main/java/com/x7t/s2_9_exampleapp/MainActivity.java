62dae10bb46c271b13055f99fac26de7beae5182
xavier7t <32859092+xavier7t@users.noreply.github.com>
2022-03-25 01:07:15 -0400

    s2_14_buttons

--- s2_9_exampleapp/app/src/main/java/com/x7t/s2_9_exampleapp/MainActivity.java
+++ s2_9_exampleapp/app/src/main/java/com/x7t/s2_9_exampleapp/MainActivity.java
@@ -3,8 +3,14 @@ package com.x7t.s2_9_exampleapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void onClick_start (View view) {
+        Log.i("start","Let’s get started.");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
