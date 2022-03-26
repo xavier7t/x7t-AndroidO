59e1daba3119425b98908495d9496a0c7f7392e9
xavier7t <32859092+xavier7t@users.noreply.github.com>
2022-03-26 18:09:33 -0400

    s2_20_images

--- s2_9_exampleapp/app/src/main/java/com/x7t/s2_9_exampleapp/MainActivity.java
+++ s2_9_exampleapp/app/src/main/java/com/x7t/s2_9_exampleapp/MainActivity.java
@@ -6,10 +6,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     public void onClick_start (View view) {
 //        EditText nameEditText = (EditText) findViewById(R.id.nameEditText);
         Log.i("start","Let’s get started.");
@@ -29,6 +29,23 @@ public class MainActivity extends AppCompatActivity {
             Log.i("Registration Succeeded", "Username: " + username + "; Password: " + password + ".");
             Toast.makeText(this,"Welcome "+ username +" !",Toast.LENGTH_SHORT).show();
         }
+    }
+
+    Integer tag = 0;
+    public void onClick_switchbg (View view) {
+        Log.i("", "onClick_switchbg: clicked." + tag);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setTag(tag);
+        Object imageViewTag = imageView.getTag();
+        if (imageViewTag.equals(2)) {
+            imageView.setImageResource(R.drawable.imglavender1);
+            tag = 1;
+        } else {
+            imageView.setImageResource(R.drawable.imglavender2);
+            tag = 2;
+        }
+
+        Log.i("", "onClick_switchbg: " + tag);
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
