e6eb047d107311eab397887ee37382fed63a2d3e
xavier7t <32859092+xavier7t@users.noreply.github.com>
2022-03-26 16:28:30 -0400

    s2_16_textfields

--- s2_9_exampleapp/app/src/main/java/com/x7t/s2_9_exampleapp/MainActivity.java
+++ s2_9_exampleapp/app/src/main/java/com/x7t/s2_9_exampleapp/MainActivity.java
@@ -5,13 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     public void onClick_start (View view) {
+//        EditText nameEditText = (EditText) findViewById(R.id.nameEditText);
         Log.i("start","Let’s get started.");
+//        Log.i("Username",nameEditText.getText().toString());
     }
 
+    public void onClick_signup (View view) {
+        EditText nameEditText = (EditText) findViewById(R.id.nameEditText);
+        EditText pwEditText = (EditText) findViewById(R.id.pwEditText);
+        String username = nameEditText.getText().toString();
+        String password = pwEditText.getText().toString();
+        if (username.equals("") || password.equals("")) {
+            Log.e("Registration Failed", "Please enter both username and password.");
+        } else {
+            Log.i("","Signing you up...");
+            Log.i("Registration Succeeded", "Username: " + username + "; Password: " + password + ".");
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
