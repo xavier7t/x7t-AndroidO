e7b9296b315e56bca60cd17e9d55ace82367812a
xavier7t <32859092+xavier7t@users.noreply.github.com>
2022-03-27 00:12:30 -0400

    s3_25_hellowordwithjava

---
+++ s3_java/s3_25_hellowordwithjava/s3_25_hellowordwithjava.java
@@ -0,0 +1,13 @@
+public class s3_25_hellowordwithjava { //creating a class, with a public access-level
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+/*
+1. Class: a template to create instances for certain types of objects.
+2. OOP - codes are based on instances of objects (created from classes).
+3. Any java program must starts with a class definition.
+4. Public - Access level => defining objects that are accessible anywhere within the program
+5. Static - described fields are fixed across instances of a common class
+ */
